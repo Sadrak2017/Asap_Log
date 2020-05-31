09d027848736b28818bd108f7971a2286ffe33a3
Sadrak Silva <sadrak.silva@saastec.com.br>
2020-05-31 17:20:00 -0300

    requisições ajax para viabilizar busca dinâmica

--- src/main/resources/static/js/custom.js
+++ src/main/resources/static/js/custom.js
@@ -1,18 +1,39 @@
 $( document ).ready(function() {
- 	
+	$("#alertRecisao").css("display", "none");
+	function closeAlert(noneOrBlock){
+	  $("#alertRecisao").css("display", noneOrBlock);
+	}
+	
 	$("#update").click(function(event){
 		$(this).val("EDITAR");
 	});
 	
 	$("#delete").click(function(event){
-		$(this).val("EXCLUIR");
+		if($("#_contrato").val()>0){
+			closeAlert("block");
+		    event.preventDefault(); // cancela operação
+		}else{
+		   $(this).val("EXCLUIR");
+		   ajaxDelete();
+		   closeAlert("none");
+		}
 	});
+	
+	
 	
 	$("#deleteClient").click(function(event){
 	    event.preventDefault();
 	    ajaxGet();
 	});
-	  
+	
+	function ajaxDelete() {
+	  $.getJSON(window.location + "/../excluir/"+ $('#selectClient').val(), {
+	    }, function(result) {
+	    	if(result.status !=  null){
+	    		location.reload();
+	    	}
+	    }); 
+	}
 	  
 	// DO GET
 	function ajaxGet(){
@@ -45,10 +66,15 @@ $( document ).ready(function() {
 		function() {
 		  $.getJSON(window.location + "/../buscar/"+ $(this).val(), {
 		    }, function(result) {
+		    	closeAlert("none");
+		    	ativos = 0;
+		    	index = 0;
 		        setVal(result.data, "_");
 		        if(result.inner != "undefined") {
 		          $('#contrato').empty();
+		      	  $("#_contrato").val("");
 		          $.each(result.inner, function(i, html){
+		        	$("#lbContratos").text("("+result.inner.length+") Contratos ");
 		        	html += '<option value="' + html.id + '" >'
 		        	   + 'Cód.: '+ html.nrApolice.toUpperCase()
 		        	   + ' - '
@@ -56,8 +82,13 @@ $( document ).ready(function() {
 		        	   + ' -  Veículo: '
 		        	   + html.placa.toUpperCase() 
 		        	   +'</option>';
-		            $('#contrato').append(html)
-		          });
+		            $('#contrato').append(html);
+		        	if(result.inner[index].inativo === false){
+			          ativos++;
+			        }
+			        $("#_contrato").val(ativos);
+			          index++;
+		          });	  
 		        }else{
 		          $("#contrato").html("<option>Nenhuma apólice relacionado ao cliente!</option>");
 		        }
@@ -65,12 +96,33 @@ $( document ).ready(function() {
 		}
 	);
 	
-	$('#findCliente').click(
-		function(event) {
-		  $.getJSON(window.location + "/../like/"+ $('#inputSearch').val(), {
+	$('#findCliente').click(function(event){
+	    event.preventDefault();
+	    $.ajax({
+	      type : "GET",
+	      url : window.location + "/../ilike/" + new String($('#inputSearch').val())+" ",
+	      success: function(result){
+	        $('#inputSearch').val("");
+	        $('#_rSelectClient').empty();
+	        if(result.data.length > 1){
+	           $.each(result.data, function(i, info){
+	        	  info += '<option value="' + info.id + '" >'
+	        	    + info.name + ' -  CPF:'+ info.cpf + '</option>';
+	              $('#_rSelectClient').append(info);
+	           });
+	        }
+	        setDados(result.data[0]);	         
+	       } 
+	    });  
+	});
+	
+	$('#_rSelectClient').change(
+		function() {
+		  $("#resultado").text("");
+		  $.getJSON(window.location + "/../buscar/"+ $(this).val(), {
 		    }, function(result) {
-		        setVal(result.data, "_r");
-		    }); 
+		      setDados(result.data);
+	      });
 		}
 	);
 
@@ -80,8 +132,17 @@ $( document ).ready(function() {
 	});
 	
 })
-	
-	
+
+function setDados(data){
+  $("#resultado").text(
+	"Nome:   " + data.name + "\n " +
+	"CPF:    " + data.cpf  + "\n " +
+	"Cidade: " + data.city + "\n " +
+	"Placa:  " + data.placa + "\n " +
+	"UF:     " + data.uf
+  );
+}
+
 function setVal(data, aux){
   $("#"+aux+"name").val(data.name).trigger('change');
   $("#"+aux+"city").val(data.city).trigger('change');
